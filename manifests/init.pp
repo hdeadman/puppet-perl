ae3b4481e6b6214084dd7788984423293ed1a9fb
fatmcgav <gavin.williams@weareact.com>
2013-01-30 12:06:23 +0000

    Updated init.pp to work with Perl 5.10 and our env...

--- manifests/init.pp
+++ manifests/init.pp
@@ -24,8 +24,8 @@ class perl (
 		}
 	} else {
 		package{$package:
-			ensure 	=> $ensure,
-			version => $version
+			ensure 	=> $version
+			#version => $version
 		}
 		package{$pmtools_package:
 			ensure => $ensure,
@@ -33,28 +33,49 @@ class perl (
 	}
 
 if $ensure == installed {
-		exec{'configure_cpan':
-			command	=> "/usr/bin/cpan <<EOF
-yes	
-yes
-no
-no
-${cpan_mirror}
+#		exec{'configure_cpan':
+#			command	=> "/usr/bin/cpan <<EOF
+#yes	
+#yes
+#no
+#no
+#${cpan_mirror}
+#
+#yes
+#quit
+#EOF",
+#			creates => "/root/.cpan/CPAN/MyConfig.pm",
+#			require => Package[$package],
+#			timeout => 600,
+#		}
 
-yes
-quit
-EOF",
-			creates => "/root/.cpan/CPAN/MyConfig.pm",
-			require => Package[$package],
-			timeout => 600,
-		}
+/*
+          file{'create_root_cpan':
+            path    => '/root/.cpan',
+            ensure  => directory
+          }
+          ->
+          file{'create_root_cpan_cpan':
+            path    => '/root/.cpan/CPAN',
+            ensure  => directory
+          }
+          ->
+          file{'configure_cpan':
+            path     => '/root/.cpan/CPAN/MyConfig.pm',
+            content  => template("perl/MyConfig.pm.erb")
+          }
+*/
+          file{'configure_shared_cpan': 
+            path     => '/usr/share/perl5/CPAN/Config.pm',
+            content  => template("perl/MyConfig.pm.erb")
+          }
 
 	  exec{"install_pmuninstall":
-	  	path 		=> ['/usr/bin/','/bin'],
-	   	command => "cpan -i App::pmuninstall",
-	   	unless 	=> "perl -MApp::pmuninstall -e 'print \"App::pmuninstall loaded\"'",
-	   	timeout => 600,
-	   	require => [Package[$perl::package],Exec['configure_cpan']],
+	  	path     => ['/usr/bin/','/bin'],
+	   	command  => "cpan -i App::pmuninstall",
+	   	unless 	 => "perl -MApp::pmuninstall -e 'print \"App::pmuninstall loaded\"'",
+	   	timeout  => 600,
+	   	require  => [Package[$perl::package],File['configure_shared_cpan']],
 	  }
 	}
 }
